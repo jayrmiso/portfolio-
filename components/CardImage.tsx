0e363bf82474242ddd8bc01353f9920abb560865
Arjay Ramiso <arjayramiso@gmail.com>
2026-05-22 13:18:08 +1000

    updated reusable cardimage styles'

---
+++ components/CardImage.tsx
@@ -0,0 +1,39 @@
+import {Card, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
+import React from "react";
+import {Badge} from "@/components/ui/badge";
+
+export interface CardImageProps {
+  src: string;
+  title: string;
+  description: string
+  badges: string[]
+}
+
+export default function CardImage({ src, title, description, badges = ["Badge A", "Badge B", "Badge C"] }: CardImageProps) {
+  return (
+    <Card className="border-b border-border h-full">
+      <img
+        src={src || "https://images.unsplash.com/photo-1497366754035-f200968a6e72?auto=format&fit=crop&w=1200&q=80"}
+        alt={description}
+        className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
+      />
+      <CardHeader>
+        <CardTitle>
+          <span>{title || 'Placeholder'}</span>
+        </CardTitle>
+        <CardDescription className={"space-y-6"}>
+          <p className={"whitespace-normal hyphens-auto"}>{description || 'Lorem ipsum'}</p>
+          <div className={"flex items-center gap-3"}>
+            {
+              badges.map((badge, index) => (
+                <Badge key={index} className={"rounded-full px-4 py-4 font-semibold transition-all duration-200 hover:-translate-y-1 hover:scale-105 hover:shadow-lg"}>
+                  {badge}
+                </Badge>
+              ))
+            }
+          </div>
+        </CardDescription>
+      </CardHeader>
+    </Card>
+  )
+}
